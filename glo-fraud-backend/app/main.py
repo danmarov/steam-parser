de51e43f15b1c82d0b146b6d1f0b1f546f0d422b
Danil <danil@Admins-MacBook-Pro.local>
2025-03-12 09:39:22 +0200

    feat: disable cors on backend

--- glo-fraud-backend/app/main.py
+++ glo-fraud-backend/app/main.py
@@ -1,11 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app.routes import auth, profile
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,  
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(profile.router)
